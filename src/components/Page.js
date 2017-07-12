7ddecc6f22daa2e05db762eb3c5c43eec307dd79
Daniel2218 <daniel_lucia22@hotmail.com>
2017-07-12 19:52:45 -0400

    Added TableHeader and Modal.js as sub components of Table.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,43 +3,14 @@ import ReactDOM from "react-dom";
 import PageSection from "./PageSection.js";
 import Stat from "./Stat.js";
 import Table from "./Table.js";
-import TableHeader from "./TableHeader.js";
-import Modal from "./Modal.js";
 
 class Page extends React.Component {
   constructor(props) {
     super(props);
-    this.toggleModal = this.toggleModal.bind(this);
-    this.state = {
-      showModal: false
-    };
-  }
-
-  toggleModal() {
-    this.setState((prevState) => ({
-      showModal: !prevState.showModal
-    }));
-  }
-
-  getTableHeaders() {
-    var currentPage = this.props.currentPage;
-    var th = [];
-
-    if(currentPage === "Applications") {
-      th = ["Applicant ID","First Name","Last Name","Hacks","Status"];
-    } else if (currentPage === "Prizes") {
-      th = ["Name","Description","Obtained By","Donated By"];
-    } else if (currentPage === "Sponsors") {
-      th = ["Name","Email","Phone","Donation Amount","Donation Recieved"];
-    } else if (currentPage === "Users") {
-      th = ["Name","Email","Phone","Postion"];
-    }
-    return th;
   }
 
   render() {
     const currentPage = this.props.currentPage;
-    const tableHeaders = this.getTableHeaders();
     var page = "";
 
     if (currentPage === "Schedule") {
@@ -47,13 +18,8 @@ class Page extends React.Component {
               </PageSection>;
     } else {
       page = <PageSection currentPage={this.props.currentPage}>
-                <TableHeader currentPage={this.props.currentPage}
-                             onButtonClick={this.toggleModal} />
                 <Table currentPage={this.props.currentPage}
-                       tableHeaders={tableHeaders} />
-                {this.state.showModal
-                 && <Modal tableHeaders={tableHeaders}
-                          toggleModal={this.toggleModal}/>}
+                       onButtonClick={this.toggleModal} />
               </PageSection>;
     }
     return page;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,30 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TableHeader from './TableHeader.js';
+import Modal from "./Modal.js";
 
 class Table extends React.Component {
   constructor(props){
     super(props);
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.state = {
+      showModal: false
+    };
+  }
+
+  toggleModal() {
+    this.setState((prevState) => ({
+      showModal: !prevState.showModal
+    }));
+  }
+
+  getTableHeaders() {
+    var currentPage = this.props.currentPage;
+    var th = [];
+
+    if(currentPage === "Applications") {
+      th = ["Applicant ID","First Name","Last Name","Hacks","Status"];
+    } else if (currentPage === "Prizes") {
+      th = ["Name","Description","Obtained By","Donated By"];
+    } else if (currentPage === "Sponsors") {
+      th = ["Name","Email","Phone","Donation Amount","Donation Recieved"];
+    } else if (currentPage === "Users") {
+      th = ["Name","Email","Phone","Postion"];
+    }
+    return th;
   }
 
   render() {
     const currentPage = this.props.currentPage;
-    const listOfHeaders = this.props.tableHeaders.map((header) =>
+    const listOfHeaders = this.getTableHeaders().map((header) =>
       <th>{header}</th>
     );
 
     return (
-      <table>
-        <tbody>
-          <tr>{listOfHeaders}</tr>
-          <tr className="tr-color">
-              <td>Daniel</td>
-              <td>Lucia</td>
-              <td>14dvl@queensu.ca</td>
-              <td>Lucia</td>
-              <td>14dvl@queensu.ca</td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <TableHeader currentPage={this.props.currentPage}
+                     onButtonClick={this.toggleModal} />
+        <table>
+          <tbody>
+            <tr>{listOfHeaders}</tr>
+            <tr className="tr-color">
+                <td>Daniel</td>
+                <td>Lucia</td>
+                <td>14dvl@queensu.ca</td>
+                <td>Lucia</td>
+                <td>14dvl@queensu.ca</td>
+            </tr>
+          </tbody>
+        </table>
+        {this.state.showModal && <Modal tableHeaders={this.getTableHeaders()} toggleModal={this.toggleModal}/>}
+      </div>
     );
   }
 }
